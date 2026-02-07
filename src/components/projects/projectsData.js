b01bd31985670256cddcfce19eb7c2b9ea30fbf3
I'm Harshit Sharma <143705769+HarshitOnLoop@users.noreply.github.com>
2026-02-07 19:38:03 +0530

    Update projectsData.js

--- src/components/projects/projectsData.js
+++ src/components/projects/projectsData.js
@@ -13,7 +13,7 @@ longDesc: "Full-stack HR tool featuring secure authentication, payroll automatio
   {
     
     id: 2,
-    img: "src/components/projects/imgp/eb.png",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjxEp_JeXDDJ9fK_BWwc32hWA17q3tpSFDIPSQwiAzhw4LlSM2qJNcVf0msBxR5BFC8V_txoJTxrOcBGKsPL6J3Vj0MLVlISXWsX5CuPKgUlsGbl_zXDX_ESscJJAL2DHB3dM54iAB41WcAF3P8SDYOTEPPaHF40Qk4sm44MRhVHnsWEjOtSoRvL3qtZGWN/s320/eb.png",
     title: "EduBuddy 📚",
     desc: "A collaborative student platform for note sharing and group study goals.",
     longDesc: "Full-stack educational hub featuring real-time chat, collaborative note sharing, and peer-to-peer study goal tracking.",
@@ -23,7 +23,7 @@ longDesc: "Full-stack HR tool featuring secure authentication, payroll automatio
   },
   {
    id: 3, // or whichever ID follows your sequence
-    img: "src/components/projects/imgp/pp.png",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjEFQoXfmkr8wRGlAkEWqe5W37F3xpkSHcnDJH8lU2gNjgHRA7f-1097ouBfkVKGYJqsvd2dDNmg5og0Npx135TXmCQ21u-Rk0UJNsoEW3e-8Zbd87aoXBatSQYHV5GsmOEezO3UzBEDSef9yZtYe1YKbHhftYlLwrdBQDjrc0-UDRaBoPEHA9iFHZvkJSY/s320/pp.png",
     title: "PassportPro ✈️",
     desc: "A streamlined digital solution for modern passport application and tracking.",
     longDesc: "Efficient management system for document verification, real-time application tracking, and secure user data handling.",
@@ -32,7 +32,7 @@ longDesc: "Full-stack HR tool featuring secure authentication, payroll automatio
   },
   {
     id: 4,
-    img: "src/components/projects/imgp/lw.png",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhFsLFDx7V49Hs4c4AgZlQUUP6cJw7_vx9m8kbVCiCkumI6t4txSCAEJ87FKAGmETZep_zEH_ysbqX_hXjAlbGA3p9W2cvBlUj-hV1rSn0BQZThZgNh-6WJhTcOA5cHdkUhMDNp7xjIw8GcW-ck_BD5J57NA6YIBxlDtpdVvkYv_vMHq5ehMhbrN-ovhqNq/s320/lw.png",
     title: "Easy Learn 🌍",
     desc: "An interactive platform for mastering new languages through community-led learning.",
     longDesc: "Feature-rich educational site with expert-curated courses, video flashcards, and global community interaction for language fluency.",
@@ -41,7 +41,7 @@ longDesc: "Full-stack HR tool featuring secure authentication, payroll automatio
 },
 {
     id: 5,
-    img: "src/components/projects/imgp/sr.png",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjt6aSB7sjCB30Tdovkrrsz-6FgnPSsggGy0xrJMZYKSLXO18zKwxN79_Xd4x2wpPcAOjms9isG9_EdFJAJb2K3Xj9NlyAPbFvhnuGVnxrhq0Ty_ghtIyBp-Eb_K1wlFBbylS-S7Cssuj7frPhFAh_3RFhXz29mU3FC_MH9I9EpE5CUKgON6CbwFr5vGi4Y/s320/sr.png",
     title: "Screen Recorder 🎥",
     desc: "A browser-based tool to capture and save screen recordings instantly.",
     longDesc: "Native recording app utilizing the MediaRecorder API to capture high-quality screen video directly within the browser.",
@@ -50,7 +50,7 @@ longDesc: "Full-stack HR tool featuring secure authentication, payroll automatio
 },
 {
     id: 5,
-    img: "src/components/projects/imgp/vr.png",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGrcAPYLLJL9xbpl5uhRzTJIjJ9NyfwXfzUMPy7ozZ_Pa8gxwZSkXvlOSxZLru2HlHK0uPdL3eO-ai3-qDLyKaJ2xXzwupP_cWd8nNWCJhcHWuhjdk179bGmrggiGnwPMdl8lvK8hFTej1ZjdDFwC_KLUzyACz9eLGLOqtyIWFnwruaYvfzf4u3xmTYSXZ/s320/vr.png",
     title: "WebRTC Video Call 📞",
     desc: "Real-time video and audio communication platform built directly for the browser.",
     longDesc: "High-performance communication app utilizing WebRTC for seamless, low-latency peer-to-peer video streaming and audio connectivity.",
